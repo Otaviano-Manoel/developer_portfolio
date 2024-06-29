60b80263b9cd8998a40dac6ad2611432f2560541
Otaviano Manoel <otavianomanoelvasconcelos@gmail.com>
2024-06-29 15:27:54 -0300

    pagina principal (src/page/App.tsx) feito

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import './App.css';
+import '../assets/style/reset.scss';
+import '../assets/style/_variables.scss';
+import '../assets/style/_mixins.scss';
+import styled from './App.module.scss';
+import ring from '../assets/images/pattern-rings.svg';
+import circle from '../assets/images/pattern-circle.svg';
+import classNames from 'classnames';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styled['app']}>
+      <img className={classNames(styled.ring1)} src={ring} alt="Ring" />
+      <img className={classNames(styled.ring2)} src={ring} alt="Ring" />
+      <img className={classNames(styled.ring3)} src={ring} alt="Ring" />
+      <div data-testid='container-page' className={styled['container']}>
+        <img className={styled['circle']} src={circle} alt="Circle" />
+      </div>
     </div>
   );
 }
--- src/page/app.test.tsx
+++ src/page/app.test.tsx
@@ -2,8 +2,31 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('verificar a existencia dos alts e a quantidade de aneis', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const elements = screen.getAllByAltText(/Ring/i);
+
+  //verifica se a quantidade de elementos é igual a 3
+  expect(elements.length).toEqual(3);
+
+  //verifica se cada elemento está presente no documento
+  elements.forEach((e) => {
+    expect(e).toBeInTheDocument();
+  })
+});
+
+test('verificar a existencia do circulo', () => {
+  render(<App />);
+  const element = screen.getByAltText(/Circle/i);
+
+  //verifica se cada elemento está presente no documento
+  expect(element).toBeInTheDocument();
+});
+
+test('verificar a existencia do campo para adicionar as seções da pagina', () => {
+  render(<App />);
+  const element = screen.getByTestId(/container-page/i);
+
+  //verifica se cada elemento está presente no documento
+  expect(element).toBeInTheDocument();
 });
